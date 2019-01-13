42d846a0aa860bc4d97b5cbe01aef82ab13e5290
hanna <hanna@192.168.1.16>
2019-01-13 14:06:26 -0500

    String and Integer arrays for the project

---
+++ SalesForce/src/gitTutorial/localAccounts.java
@@ -0,0 +1,14 @@
+package gitTutorial;
+
+public class localAccounts {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+
+		String []accounts = {"checking", "Saving", "TaxFreeAccount"};
+		System.out.println("The accounts that is found for this client is "+ accounts);
+	}
+
+}
---
+++ SalesForce/src/gitTutorial/localSales.java
@@ -0,0 +1,17 @@
+package gitTutorial;
+
+import java.util.Scanner;
+
+public class localSales {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	
+		int []x= {2,3,4};
+		
+		System.out.println("The value of the array is " + x);
+		
+	}
+
+}
